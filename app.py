0267ddd1d634df22250709c722471db5c18964d5
scoop <jjagy300@gmail.com>
2021-10-25 21:43:26 +0700

    up

--- app.py
+++ app.py
@@ -4,48 +4,103 @@ import os
 from flask import Flask, jsonify, request
 from connecttion import get_database_by_name
 from pprint import pprint
-# import csv
-# from decisiontree import my_main
+import csv
+from decisiontree import my_main
 
-# import pandas as pd
+import pandas as pd
 import numpy as np
-# from sklearn.metrics import confusion_matrix
-# from sklearn.model_selection import train_test_split
-# from sklearn.tree import DecisionTreeClassifier
-# from sklearn.metrics import accuracy_score
-# from sklearn.metrics import classification_report
-# from sklearn.tree import export_text
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report
+from sklearn.tree import export_text
 
-# from sklearn import tree
-# from matplotlib import pyplot as plt
-# import graphviz
+from sklearn import tree
+from matplotlib import pyplot as plt
+import graphviz
 import joblib
 
 app = Flask(__name__)
 
-@app.route('/prediction')
-def hello_world():  # put application's code here
+def create_csv_train(num):
+    db = get_database_by_name('Roomsystem')
+    col_static = db['static']
+    # sum = num
+    # print("จำนวน record ==> ",sum)
+    statics = col_static.find().limit(num)
+    header = ['_id', 'datetime', 'luminance', 'motion', 'temperature', 'status']
+    with open('data_train.csv', 'w', encoding='UTF8') as f:
+        writer = csv.writer(f)
+
+        # write the header
+        writer.writerow(header)
+
+        # write the data
+        for static in statics:
+            # print(static['_id'])
+            writer.writerow(
+                [static['_id'],
+                 static['datetime'],
+                 static['luminance'],
+                 static['motion'],
+                 static['temperature'],
+                 'Full' if static['label'] == 'กำลังถูกใช้' else 'Empty'
+                 ]
+            )
+
+@app.route('/trainnig/<int:num>')
+def train_ning(num):
+    # sum = num
+    # print("จำนวน record ==> ",sum)
+    [confusion, accuracy, report] = my_main(num)
+    create_csv_train(num)
+    json_str = json.dumps(
+        {'message': 'OK','Record data':num ,'confusion': confusion, 'accuracy': accuracy, 'report': report},
+        ensure_ascii=False, default=str)
+    return app.response_class(json_str, mimetype='application/json')
+
+@app.route('/prediction/<int:number>')
+def hello_world(number):
     db = get_database_by_name('Roomsystem')
     col_infomation1 = db['information']
     informotions1 = col_infomation1.find()
-    # print("==>",informotions1.count())
+    # print("==>",informotions1)
     count_data = informotions1.count()
-    # print(count_data)
+    # count_data = col_infomation1.count_documents({})
+    print(count_data)
     datasensor = np.zeros((count_data,3))
     # print(datasensor)
     data = []
 
     # #เรียกโมเดล
-    path_model = 'jubjang_model.pp'
+    if number == 5:
+        path_model = 'model_500.ph5'
+    elif number == 4:
+        path_model = 'model_400.ph5'
+    elif number == 3:
+        path_model = 'model_300.ph5'
+    elif number == 2:
+        path_model = 'model_200.ph5'
+    # path_model = 'model_500.ph5'
     loaded_model = joblib.load(path_model)
 
     for i,data in enumerate(informotions1):
         datasensor[i][0] = data['luminance']
         datasensor[i][1] = data['motion']
         datasensor[i][2] = data['temperature']
+
     currentId = data['_id']
+    currentId1 = data['luminance']
+    currentId2 = data['motion']
+    currentId3 = data['temperature']
+    print(currentId)
+    print(currentId1)
+    print(currentId2)
+    print(currentId3)
     #last index
     value = datasensor[count_data-1]
+    # print(value)
     
     re = ""
     status = 0
@@ -63,7 +118,7 @@ def hello_world():  # put application's code here
         status = 1
 
     json_str = json.dumps(
-        {'message': 'OK','Result':listToStr, 'status': status})
+        {'message': 'OK','Model':number,'Result':listToStr, 'status': status})
     return app.response_class(json_str, mimetype='application/json')
 
 if __name__ == '__main__':
--- decisiontree.py
+++ decisiontree.py
@@ -1,107 +1,66 @@
-#region prediction old
-# import numpy as np
-# import pandas as pd
-# from sklearn.metrics import confusion_matrix
-# from sklearn.model_selection import train_test_split
-# from sklearn.tree import DecisionTreeClassifier
-# from sklearn.metrics import accuracy_score
-# from sklearn.metrics import classification_report
-# from sklearn.tree import export_text
+import numpy as np
+import pandas as pd
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report
+from sklearn.tree import export_text
 
-# from sklearn import tree
-# # from matplotlib import pyplot as plt
-# import graphviz
-# import os
+from sklearn import tree
+# from matplotlib import pyplot as plt
+import graphviz
+import os
 
+import joblib
 
-# def import_data():
-#     dataset = pd.read_csv('data_train.csv', sep=',')
+def my_main(num):  
+    # 1) Select data --> load data
+    dataset = pd.read_csv('data_train.csv',sep=',')
+    # Printing the dataswet shape
+    print("Dataset Length: ",len(dataset))
+    print("Dataset Shape: " ,dataset.shape)
+    # Printing the dataset obseravtions
+    print("Dataset: ",dataset.head())
 
-#     # Printing the dataswet shape
-#     print("Dataset Length: ", len(dataset))
-#     print("Dataset Shape: ", dataset.shape)
-#     # Printing the dataset obseravtions
-#     print("Dataset: ", dataset.head())
-#     return dataset
+    # 2) Split data
+    X = dataset.values[:,2:5] #row dataset
+    Y = dataset.values[:,5] #Class column ที่ 4
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, Y, test_size= 0.3, random_state= 100) #แบ่งข้อมูล 70:30 เรียน:ทดสอบ
 
+    # 3) Model data --> Tree generating
+    clf_entropy = DecisionTreeClassifier(criterion= "entropy",
+                random_state= 100, max_depth= 3, min_samples_leaf= 5)#สร้างต้นไม้ตัดสินใจ 
+    a = clf_entropy.fit(X_train, y_train) #เรียนรู้ข้อมูล
+    print("Model : ",a)
 
-# # Function to split the dataset
-# def split_dataset(dataset):
-#     # Separating the target variable
-#     X = dataset.values[:, 2:5]  # row dataset
-#     Y = dataset.values[:, 5]  # Class column ที่ 4
+    # 4) Function to calculate accuracy
+    y_pred = clf_entropy.predict(X_test)
+    confusion = confusion_matrix(y_test, y_pred)
+    accuracy = accuracy_score(y_test, y_pred) * 100
+    report = classification_report(y_test, y_pred)
+    print("\n")
+    print("Confusion matrix : ",confusion)
+    print("Accuracy : ",accuracy)
+    print("Report : ",report)
+    # print('จำนวนแถว', num)
 
-#     # Splitting the dataset into train and test
-#     X_train, X_test, y_train, y_test = train_test_split(
-#         X, Y, test_size=0.3, random_state=100)
+    if num == 500:
+        filename = 'model_500.ph5'
+        joblib.dump(clf_entropy,filename)
+    elif num == 400:
+        filename = 'model_400.ph5'
+        joblib.dump(clf_entropy,filename)
 
-#     return X, Y, X_train, X_test, y_train, y_test
+    elif num == 300:
+        filename = 'model_300.ph5'
+        joblib.dump(clf_entropy,filename)
+    elif num == 200:
+        filename = 'model_200.ph5'
+        joblib.dump(clf_entropy,filename)
+    else:
+        print("not model!")
+        
+    return [confusion, accuracy, report] 
 
-
-# def train_using_entropy(X_train, X_test, y_train):
-#     # Decision tree with entropy
-#     clf_entropy = DecisionTreeClassifier(criterion="entropy",
-#                                          random_state=100, max_depth=3, min_samples_leaf=5)
-
-#     # Performing training
-#     clf_entropy.fit(X_train, y_train)
-#     return clf_entropy
-
-
-# def prediction(X_test, clf_object):
-#     # Predicton on test with giniIndex
-#     y_pred = clf_object.predict(X_test)
-#     print("Predicted values: ")
-#     print(y_pred)
-#     return y_pred
-
-
-# # Function to calculate accuracy
-# def cal_accuracy(y_test, y_pred):
-#     print("Confusion vatrix : ",
-#           confusion_matrix(y_test, y_pred))
-
-#     print("Accuracy : ",
-#           accuracy_score(y_test, y_pred) * 100)
-
-#     print("Report : ",
-#           classification_report(y_test, y_pred))
-
-
-# def my_main():
-#     # Building Phase
-#     data = import_data()
-#     # print("==> ",data)
-#     X, Y, X_train, X_test, y_train, y_test = split_dataset(data)
-#     clf_entropy = train_using_entropy(X_train, X_test, y_train)
-
-#     # Operational Phase
-#     print("<------------------------------ Result Using Entropy ---------------------------->")
-#     # Prediction using entropy
-#     y_pred_entropy = prediction(X_test, clf_entropy)
-#     cal_accuracy(y_test, y_pred_entropy)
-
-#     # Target_names = ['Full', 'Empty']
-#     # Feature_names = ['luminance', 'motion', 'temperature']
-#     # dot_data = tree.export_graphviz(clf_entropy, out_file=None, feature_names=Feature_names,
-#     #                                 class_names=Target_names, filled=True, rounded=True,
-#     #                                 special_characters=True)  # Filled คือ เติมสี, Rounded คือ ทำขอบมน
-#     # graph = graphviz.Source(dot_data)
-#     # graph.render('/content/drive/MyDrive/Python/Tree/dtree_render1', view=True)
-#     # tree.export_graphviz(clf_entropy, out_file='/content/drive/MyDrive/Python/Tree/Tree.dot1')
-#     # graph.render('dtree_render1', view=True)
-#     # tree.export_graphviz(clf_entropy, out_file='Tree.dot1')
-#     #
-# #endregion
-    
-#     # # 4) Use the model
-#     confusion = confusion_matrix(y_test, y_pred_entropy)
-#     accuracy = accuracy_score(y_test, y_pred_entropy) * 100
-#     report = classification_report(y_test, y_pred_entropy)
-
-#     dataTest = pd.read_csv('data_test.csv', sep=',')
-#     modelUse = dataTest.values[:, 2:5]  # row dataset
-#     Result = clf_entropy.predict(modelUse)
-#     print(Result)
-#     return [Result, confusion, accuracy, report]
-#endregion
